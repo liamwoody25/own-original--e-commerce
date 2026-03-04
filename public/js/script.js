dfe8f2a716ac3f0c275c4b34c51eff8f8029d6bc
liam woodward <liamwoodward20@outlook.com>
2026-03-04 14:00:23 +0000

    adding the product numbers to the bag in html and javascript

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,38 @@
+const purchaseBtn = document.querySelectorAll('.item-btn')
 const burgerToggle = document.querySelector('.ham-burger');
 const menuDropDown = document.querySelector('.nav-bar');
 
-function hamBurgerMenu() {
-  if (burgerToggle.classList.toggle('active')) {
-    menuDropDown.style.display = 'flex'
-  } else {
-    menuDropDown.style.display = 'none'
-  }
+function sendProductToCart(i) {
+ let output = document.querySelectorAll('#bag-output')[0];
+ let result = Number(output.innerText) + 1;
+
+ if (result > 10) {
+  result = 0
+ }
+
+  output.innerText = result
+
+
+  
+}
+
+// function hamBurgerMenu() {
+//   if (burgerToggle.classList.toggle('active')) {
+//     menuDropDown.style.display = 'flex'
+//   } else {
+//     menuDropDown.style.display = 'none'
+//   }
+// }
+
+
+
+for (let i = 0 ; i < purchaseBtn.length ; i++ ) {
+    purchaseBtn[i].addEventListener('click', function(){
+    sendProductToCart(i)
+  })
 }
 
 
-burgerToggle.addEventListener('click', function(){
-  hamBurgerMenu()
-})
+// burgerToggle.addEventListener('click', function(){
+//   hamBurgerMenu()
+// })
